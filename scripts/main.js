b9c846c93163c3c0ad62b6922fca000f334d6427
Austin Godfrey <bondboy88@gmail.com>
2017-07-24 18:20:15 +0900

    Change to Google Search, Change to radio buttons, and add button memory

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
+  if(!localStorage.getItem("game")) {
+    $.each($("input[name='game']"), function(){
+      if($(this).val() == "+MHGen+Gen+Generations"){
+        $(this).prop('checked', true);
+      }
+    });
+  }
+  else {
+    $.each($("input[name='game']"), function(){
+      if($(this).val() == localStorage.getItem("game")){
+        $(this).prop('checked', true);
+      }
+    });
+  }
   $("#search-button").click(function(){
     var search = "+" + $("#search-text").val().split(" ").join("+");
     var game = "\"monster+hunter\"";
     $.each($("input[name='game']:checked"), function(){
       game += $(this).val();
+      localStorage.setItem("game", $(this).val());
     });
     $("#search-link").attr("href", "https://www.google.com/search?q=" + game + search);
   });
+  $("#google-button").click(function(){
+    var search = "+" + $("#search-text").val().split(" ").join("+");
+    $("#google-link").attr("href", "https://www.google.com/search?q=" + search);
+  });
   $("#search-text").keypress(function(e){
-    if(e.which == 13){//Enter key pressed
-        $('#search-button').click();//Trigger search button click event
+    if(e.which == 13){
+      e.preventDefault();
+      $("#search-button").click();
     }
-});
+  });
 });
